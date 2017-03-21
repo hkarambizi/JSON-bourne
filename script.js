e961339cf00c366bee3d060edafb0441873c25f8
hkarambizi <hkarambizi@gmail.com>
2017-03-21 00:44:28 -0400

    Mission one completed, Mission two attempt

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+//1. Mission one
+
+$('#mission_one_btn').on("click", function() {
+	$.get('https://api.myjson.com/bins/1680y')
+		.done(function(data){
+		console.log(data);
+		})
+		.fail(function(error) {
+		console.log(error)
+		})
+});
+
+//Mission Two
+
+$('mission_two_btn').on("click", function() {
+	var data = $.get('https://api.myjson.com/bins/1680y');
+	$.ajax({
+	    url:"https://api.myjson.com/bins",
+	    type:"POST",
+	    data: data.passphrase,
+	    contentType:"application/json; charset=utf-8",
+	    dataType:"json",
+	    })
+		.done(function(data){
+		console.log(data);
+		})
+		.fail(function(error) {
+		console.log(error)
+		})
+});    
